5a6ef4b54ea662135d3435f49b84d41c621bbfa5
rohitmanojpandey <rohitpandey20002017@gmail.com>
2025-09-06 15:37:38 +0530

    second commit

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from src.evaluation import evaluate_classification,feature_importance_svc
+import kagglehub
+from pathlib import Path
+# Download latest version
+def install_dataset():
+    data_path = Path(__file__).parent / 'data'
+    print(data_path, data_path.iterdir())
+    if any(data_path.iterdir()):
+        return
+    kagglehub.dataset_download("uciml/iris", path=data_path)
+
 # evaluate_classification()   #run training
+install_dataset()
 feature_importance_svc()
